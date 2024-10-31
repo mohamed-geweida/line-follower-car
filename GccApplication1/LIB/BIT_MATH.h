b611f0e97133bfdac80a05b4fc0516013ea1df86
Mohamed Geweida <geweidacodes@gmail.com>
2024-10-31 14:22:36 +0300

    Final Touch

---
+++ GccApplication1/LIB/BIT_MATH.h
@@ -0,0 +1,20 @@
+/*
+ * BIT_MATH.h
+ *
+ * Author: 0110
+ */ 
+
+
+#ifndef BIT_MATH_H_
+#define BIT_MATH_H_
+
+#define ERROR 0
+#define NO_ERROR 1
+#define NULL ((void *)0)
+
+#define SET_BIT(var, bitno) ((var) |= (1 << (bitno)))
+#define CLR_BIT(var, bitno) ((var) &= ~(1 << (bitno)))
+#define TOG_BIT(var, bitno) ((var) ^= (1 << (bitno)))
+#define GET_BIT(var, bitno) (((var) >> (bitno)) & 0x01)
+
+#endif /* BIT_MATH_H_ */
---
+++ GccApplication1/MCAL/DIO/DIO_program.c
@@ -0,0 +1,151 @@
+/*
+ * DIO_program.c
+ *
+ * Author: 0110
+ */ 
+
+#include "../../LIB/STD_TYPES.h"
+#include "../../LIB/BIT_MATH.h"
+#include "../../MCAL/DIO/DIO_interface.h"
+#include "../../MCAL/DIO/DIO_register.h"
+
+u8 DIO_u8_SetPinDirection(u8 Copy_u8Port, u8 Copy_u8Pin, u8 Copy_u8Direction)
+{
+	u8 Local_u8ErrorState = 0;
+	if ((Copy_u8Pin <= DIO_u8Pin7) && (Copy_u8Pin >= DIO_u8Pin0))
+	{
+		if (Copy_u8Direction == DIO_u8Pin_Output)
+		{
+			switch (Copy_u8Port)
+			{
+				case DIO_u8PORTA: SET_BIT(DDRA, Copy_u8Pin); break;
+				case DIO_u8PORTB: SET_BIT(DDRB, Copy_u8Pin); break;
+				case DIO_u8PORTC: SET_BIT(DDRC, Copy_u8Pin); break;
+				case DIO_u8PORTD: SET_BIT(DDRD, Copy_u8Pin); break;
+				default: Local_u8ErrorState = 1;
+			}
+		}
+		else if (Copy_u8Direction == DIO_u8Pin_Input)
+		{
+			switch (Copy_u8Port)
+			{
+				case DIO_u8PORTA: CLR_BIT(DDRA, Copy_u8Pin); break;
+				case DIO_u8PORTB: CLR_BIT(DDRB, Copy_u8Pin); break;
+				case DIO_u8PORTC: CLR_BIT(DDRC, Copy_u8Pin); break;
+				case DIO_u8PORTD: CLR_BIT(DDRD, Copy_u8Pin); break;
+				default: Local_u8ErrorState = 1;
+			}
+		}
+		else
+		{
+			Local_u8ErrorState = 1;
+		}
+	}
+	else
+	{
+		Local_u8ErrorState = 1;
+	}
+	return Local_u8ErrorState;
+}
+
+u8 DIO_u8_SetPortDirection(u8 Copy_u8Port, u8 Copy_u8Direction)
+{
+	u8 Local_u8ErrorState = 0;
+	if ((Copy_u8Direction == DIO_u8Pin_Output) || (Copy_u8Direction == DIO_u8Pin_Input))
+	{
+		switch (Copy_u8Port)
+		{
+			case DIO_u8PORTA: DDRA = Copy_u8Direction; break;
+			case DIO_u8PORTB: DDRB = Copy_u8Direction; break;
+			case DIO_u8PORTC: DDRC = Copy_u8Direction; break;
+			case DIO_u8PORTD: DDRD = Copy_u8Direction; break;
+			default: Local_u8ErrorState = 1;
+		}
+	}
+	else
+	{
+		Local_u8ErrorState = 1;
+	}
+	return Local_u8ErrorState;
+}
+
+u8 DIO_u8_SetPinValue(u8 Copy_u8Port, u8 Copy_u8Pin, u8 Copy_u8Value)
+{
+	u8 Local_u8ErrorState = 0;
+	if ((Copy_u8Pin <= DIO_u8Pin7) && (Copy_u8Pin >= DIO_u8Pin0))
+	{
+		if (Copy_u8Value == DIO_u8Pin_High)
+		{
+			switch (Copy_u8Port)
+			{
+				case DIO_u8PORTA: SET_BIT(PORTA, Copy_u8Pin); break;
+				case DIO_u8PORTB: SET_BIT(PORTB, Copy_u8Pin); break;
+				case DIO_u8PORTC: SET_BIT(PORTC, Copy_u8Pin); break;
+				case DIO_u8PORTD: SET_BIT(PORTD, Copy_u8Pin); break;
+				default: Local_u8ErrorState = 1;
+			}
+		}
+		else if (Copy_u8Value == DIO_u8Pin_Low)
+		{
+			switch (Copy_u8Port)
+			{
+				case DIO_u8PORTA: CLR_BIT(PORTA, Copy_u8Pin); break;
+				case DIO_u8PORTB: CLR_BIT(PORTB, Copy_u8Pin); break;
+				case DIO_u8PORTC: CLR_BIT(PORTC, Copy_u8Pin); break;
+				case DIO_u8PORTD: CLR_BIT(PORTD, Copy_u8Pin); break;
+				default: Local_u8ErrorState = 1;
+			}
+		}
+		else
+		{
+			Local_u8ErrorState = 1;
+		}
+	}
+	else
+	{
+		Local_u8ErrorState = 1;
+	}
+	return Local_u8ErrorState;
+}
+
+u8 DIO_u8_SetPortValue(u8 Copy_u8Port, u8 Copy_u8Value)
+{
+	u8 Local_u8ErrorState = 0;
+	if ((Copy_u8Value == DIO_u8Port_High) || (Copy_u8Value == DIO_u8Port_Low))
+	{
+		switch (Copy_u8Port)
+		{
+			case DIO_u8PORTA: PORTA = Copy_u8Value; break;
+			case DIO_u8PORTB: PORTB = Copy_u8Value; break;
+			case DIO_u8PORTC: PORTC = Copy_u8Value; break;
+			case DIO_u8PORTD: PORTD = Copy_u8Value; break;
+			default: Local_u8ErrorState = 1;
+		}
+	}
+	else
+	{
+		Local_u8ErrorState = 1;
+	}
+	return Local_u8ErrorState;
+}
+
+u8 DIO_u8_GetPinValue(u8 Copy_u8Port, u8 Copy_u8Pin, u8 *Copy_pu8Value)
+{
+	u8 Local_u8ErrorState = 0;
+	if ((Copy_u8Pin <= DIO_u8Pin7) && (Copy_u8Pin >= DIO_u8Pin0) && (Copy_pu8Value != NULL))
+	{
+		switch (Copy_u8Port)
+		{
+			case DIO_u8PORTA: *Copy_pu8Value = GET_BIT(PINA, Copy_u8Pin); break;
+			case DIO_u8PORTB: *Copy_pu8Value = GET_BIT(PINB, Copy_u8Pin); break;
+			case DIO_u8PORTC: *Copy_pu8Value = GET_BIT(PINC, Copy_u8Pin); break;
+			case DIO_u8PORTD: *Copy_pu8Value = GET_BIT(PIND, Copy_u8Pin); break;
+			default: Local_u8ErrorState = 1;
+		}
+	}
+	else
+	{
+		Local_u8ErrorState = 1;
+	}
+	return Local_u8ErrorState;
+}
