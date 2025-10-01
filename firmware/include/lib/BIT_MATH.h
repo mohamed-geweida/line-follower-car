f6dad5dbb3e6d97fbb44951d22956255a9a33ae8
Mohamed Geweida <geweidacodes@gmail.com>
2025-10-01 20:27:34 +0300

    Remove unused macros from BIT_MATH.h to simplify the code and improve clarity.

--- firmware/include/lib/BIT_MATH.h
+++ firmware/include/lib/BIT_MATH.h
@@ -1,10 +1,6 @@
 #ifndef BIT_MATH_H_
 #define BIT_MATH_H_
 
-#define ERROR 0
-#define NO_ERROR 1
-#define NULL ((void *)0)
-
 #define SET_BIT(var, bitno) ((var) |= (1 << (bitno)))
 #define CLR_BIT(var, bitno) ((var) &= ~(1 << (bitno)))
 #define TOG_BIT(var, bitno) ((var) ^= (1 << (bitno)))
