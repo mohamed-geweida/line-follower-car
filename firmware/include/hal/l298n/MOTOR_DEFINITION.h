7492298cd0c21260bfec2e04fa360206ae1db746
Mohamed Geweida <geweidacodes@gmail.com>
2025-10-01 20:14:58 +0300

    Add MOTOR_DEFINITION.h for motor and sensor pin definitions

---
+++ firmware/include/hal/l298n/MOTOR_DEFINITION.h
@@ -0,0 +1,15 @@
+#ifndef MOTOR_DEFINITION_H_
+#define MOTOR_DEFINITION_H_
+
+#define IN1  DIO_u8Pin0
+#define IN2  DIO_u8Pin1
+#define ENA  DIO_u8Pin2
+#define IN3  DIO_u8Pin3
+#define IN4  DIO_u8Pin4
+#define ENB  DIO_u8Pin5
+
+#define LEFT_IR  DIO_u8Pin6
+#define RIGHT_IR DIO_u8Pin7
+
+#endif /* MOTOR_DEFINITION_H_ */
+
