41f2ab17a2dfc7e45e9bd5ad7b1a2dc933e0a5dd
Mohamed Geweida <geweidacodes@gmail.com>
2025-10-01 20:15:17 +0300

    Add STD_TYPES.h for standard type definitions and error handling constants

---
+++ firmware/include/lib/STD_TYPES.h
@@ -0,0 +1,22 @@
+/* Mirror of original with same API */
+#ifndef STD_TYPES_H_
+#define STD_TYPES_H_
+
+typedef unsigned char u8;
+typedef unsigned short u16;
+typedef unsigned long u32;
+
+typedef signed char s8;
+typedef signed short s16;
+typedef signed long s32;
+
+typedef float f32;
+typedef double f64;
+
+#define ERROR 0
+#define NO_ERROR 1
+
+#define NULL ((void *)0)
+
+#endif /* STD_TYPES_H_ */
+
